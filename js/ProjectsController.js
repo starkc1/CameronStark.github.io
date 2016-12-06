5ad4654c7f2eab02b2742c2579f2d22684a3ffcb
starkc1 <cameronstark@StarkAir.local>
2016-12-05 20:37:56 -0500

    changed controller name

---
+++ js/ProjectsController.js
@@ -0,0 +1,11 @@
+app.controller('ProjectsController', ['$scope', function($scope)
+{
+    $scope.projects =
+    [
+        {
+            image: 'images/Project_1.png',
+            desc1: 'Basic Math Solver',
+            desc2: '(Windows 10)'
+        }
+    ];
+}]);

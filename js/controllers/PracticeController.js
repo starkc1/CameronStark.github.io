3951673e6095d264f1a44124dba51b59ec8b9ee7
starkc1 <cameronstark@StarkAir.mshome.net>
2016-12-22 12:25:52 -0500

    changed formatting of controller

--- js/controllers/PracticeController.js
+++ js/controllers/PracticeController.js
@@ -3,9 +3,9 @@ app.controller('PracticeController', ['$scope', function($scope) {
     $scope.info =
     [
         {
-            "key":"StateMachine",
-            "description":"A general purpose set of classes to implement data driven inter communicating state machines",
-            "name":"State Machine"
+            key:"StateMachine",
+            description:"A general purpose set of classes to implement data driven inter communicating state machines",
+            name:"State Machine"
 
         }
     ];

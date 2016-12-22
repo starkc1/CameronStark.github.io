b1e35e0dc137e9fc15a70d014b1e90f17df0e263
starkc1 <cameronstark@StarkAir.mshome.net>
2016-12-22 12:45:18 -0500

    added and changed angular

--- js/controllers/PracticeController.js
+++ js/controllers/PracticeController.js
@@ -1,12 +1,22 @@
-app.controller('PracticeController', ['$scope', function($scope) {
+app.controller('PracticeController', ['$scope', function ($scope) {
 
     $scope.info =
-    [
-        {
-            key:"StateMachine",
-            description:"A general purpose set of classes to implement data driven inter communicating state machines",
-            name:"State Machine"
+        [
+            {
+                key: "StateMachine",
+                description: "A general purpose set of classes to implement data driven inter communicating state machines",
+                name: "State Machine",
+                currentVersion: {
+                    major: 0,
+                    minor: 8,
+                    revision: 1,
+                    build: 1,
+                    dateStamp: "2016-12-20T00:00:00",
+                    releaseNotes: "Initial unstable preview"
+                },
+                versionHistory: null,
+                domainType: 1
 
-        }
-    ];
+            }
+        ];
 }]);

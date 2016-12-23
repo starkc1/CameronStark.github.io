4bbf4afc37c2e1d99ca55df97b757d5dfa5d2069
starkc1 <cameronstark@StarkAir.local>
2016-12-23 12:46:52 -0500

    added directive and showhide

--- js/controllers/ActualController.js
+++ js/controllers/ActualController.js
@@ -1,5 +1,9 @@
 app.controller('ActualController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
+        $scope.IsVisible = false;
+        $scope.ShowHide = function() {
+            $scope.IsVisible = $scope.IsVisible ? false : true;
+        }
     });
 });

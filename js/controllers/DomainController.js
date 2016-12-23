c67f554d51e90a1a9b9e376001559e094e3d0df3
starkc1 <cameronstark@StarkAir.local>
2016-12-23 15:38:46 -0500

    fixed typo

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,4 +1,4 @@
-app.controller('ActualController', function($scope, $http) {
+app.controller('DomainController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });

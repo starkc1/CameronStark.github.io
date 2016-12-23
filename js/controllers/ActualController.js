bc5f8dd182f042cbda49dd1b402152f710f7e1b8
starkc1 <cameronstark@StarkAir.local>
2016-12-23 15:23:12 -0500

    added more http request

--- js/controllers/ActualController.js
+++ js/controllers/ActualController.js
@@ -2,4 +2,7 @@ app.controller('ActualController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
+    $http.get("http://dev.nuviot.com/metadata/dox/domains" + info.key).then(function(response) {
+        $scope.models = response.data
+    });
 });

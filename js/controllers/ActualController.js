c7fe1281dc17a8ce345446281faf1b002a4e45d0
starkc1 <cameronstark@StarkAir.local>
2016-12-23 15:02:41 -0500

    removed not used angular

--- js/controllers/ActualController.js
+++ js/controllers/ActualController.js
@@ -1,9 +1,5 @@
-app.controller('ActualController', function ($scope, $http) {
-    $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function (response) {
+app.controller('ActualController', function($scope, $http) {
+    $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
-    }),
-    $scope.IsVisible = false;
-    $scope.ShowHide = function () {
-        $scope.IsVisible = $scope.IsVisible ? false : true;
-    }
+    });
 });

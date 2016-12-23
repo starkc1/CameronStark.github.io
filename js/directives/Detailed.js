e2bd78485836677077dd8f0006f48fc1ee058be8
starkc1 <cameronstark@StarkAir.local>
2016-12-23 12:31:03 -0500

    added new directive

---
+++ js/directives/Detailed.js
@@ -0,0 +1,13 @@
+app.directive('Detailed', function() {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'js/directives/Detailed.html',
+
+        link: function(scope, element, attrs) {
+            scope.detailed = function() {
+                
+            }
+        }
+    }
+})

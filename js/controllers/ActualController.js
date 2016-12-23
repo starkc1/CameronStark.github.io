e0af648335242ea6a18c41364c199bab15f265c2
starkc1 <cameronstark@StarkAir.local>
2016-12-23 15:26:19 -0500

    reverted controller

--- js/controllers/ActualController.js
+++ js/controllers/ActualController.js
@@ -2,7 +2,4 @@ app.controller('ActualController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
-    $http.get("http://dev.nuviot.com/metadata/dox/domains" + info.key).then(function(response) {
-        $scope.models = response.data
-    });
 });

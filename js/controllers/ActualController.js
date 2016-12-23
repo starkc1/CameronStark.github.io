0ab8bd82ddfd17fd571571cebf2dbe0a92445a79
starkc1 <cameronstark@StarkAir.local>
2016-12-23 12:51:11 -0500

    fixed? controller

--- js/controllers/ActualController.js
+++ js/controllers/ActualController.js
@@ -1,9 +1,9 @@
-app.controller('ActualController', function($scope, $http) {
-    $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
+app.controller('ActualController', function ($scope, $http) {
+    $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function (response) {
         $scope.info = response.data;
-        $scope.IsVisible = false;
-        $scope.ShowHide = function() {
-            $scope.IsVisible = $scope.IsVisible ? false : true;
-        }
-    });
+    }),
+    $scope.IsVisible = false;
+    $scope.ShowHide = function () {
+        $scope.IsVisible = $scope.IsVisible ? false : true;
+    }
 });

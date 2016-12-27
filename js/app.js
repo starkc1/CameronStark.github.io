f7702843ff36c3be854a16723c4617abe8dcb8c1
starkc1 <cms117@live.com>
2016-12-27 12:01:39 -0500

    removed routeparams

--- js/app.js
+++ js/app.js
@@ -1,14 +1,2 @@
 var app = angular.module('Portfolio',[]);
 
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProivder.
-    when('/domains', {
-        controller: 'DomainController'
-    }).
-    when('/domains/:domainKey', {
-        controller: 'ModelController'
-    }).
-    otherwise({
-       redirectTo: 'list' 
-    });
-}]);
---
+++ js/controllers/ModelController.js
@@ -0,0 +1,3 @@
+app.controller('ModelController', function($scope, $http, $routeParams) {
+    
+})

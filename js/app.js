38d606f47d57ed54dbe1306461b2a6455201fcde
starkc1 <cms117@live.com>
2016-12-27 12:29:51 -0500

    changed config

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@ var app = angular.module('Portfolio',[]);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProivder.
     when('/domains.html', {
+        templateUrl: 'domains.html',
         controller: 'DomainController'
     }).
     when('/domains.html/:domainKey', {
+        templateUrl: 'domains.html',
         controller: 'ModelController'
     }).
     otherwise({

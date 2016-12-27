e3f77c62ac35299b8463d8946448962fc1bb9475
starkc1 <cms117@live.com>
2016-12-27 15:21:14 -0500

    testing routing

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
-var app = angular.module('Portfolio',[]);
+var app = angular.module('Portfolio',['ngRoute']);
 
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/domains.html', {templateUrl: 'domains.html'});   
+    $routeProvider.when('/domains.html/id', {templateUrl: 'domains.html'});
+}]);

5a1b411adbdd039d7c34706ed5f74e9062f393dd
starkc1 <cms117@live.com>
2016-12-27 13:33:49 -0500

    added model controller

--- js/controllers/ModelController.js
+++ js/controllers/ModelController.js
@@ -1,3 +1,5 @@
-app.controller('ModelController', function($scope, $http, $routeParams) {
-    
-})
+app.controller('ModelController', function($scope, $http, $attrs) {
+    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + "id").then(function(response) {
+        $scope.models = response.data;
+    });
+});

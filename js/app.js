6eec9d55ee9cc72bcabeb9c43a20a8d5f2f68cf3
starkc1 <cms117@live.com>
2016-12-27 12:03:56 -0500

    readded routeparams

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 var app = angular.module('Portfolio',[]);
 
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProivder.
+    when('/domains', {
+        controller: 'DomainController'
+    }).
+    when('/domains/:domainKey', {
+        controller: 'ModelController'
+    }).
+    otherwise({
+       redirectTo: 'list' 
+    });
+}]);

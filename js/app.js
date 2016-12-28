1b5db8cf863103075538957a733884372749c04f
starkc1 <cms117@live.com>
2016-12-28 14:14:13 -0500

    added to config

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var app = angular.module('Portfolio',['ngRoute']);
 config(['$routeProvider','$locationProvider' , function($routeProvider, $locationProvider) {
     $routeProvider.when('/NuvIoT.html', {templateUrl: 'NuvIoT.html'});   
     $routeProvider.when('/NuvIoT.html/Models', {templateUrl: '/partials/Models.html', controller:"ModelController"});
+    $routeProvider.otherwise({redirectTo: '/NuvIoT.html'});
 
     $locationProvider.html5Mode(true);
 }]);

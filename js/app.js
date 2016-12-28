57f3ac7229dde84027eada44ebaeb1133dfac856
starkc1 <cms117@live.com>
2016-12-28 14:18:38 -0500

    testing routing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('Portfolio',['ngRoute']);
 
 config(['$routeProvider','$locationProvider' , function($routeProvider, $locationProvider) {
-    $routeProvider.when('/NuvIoT.html', {templateUrl: 'NuvIoT.html'});   
+    $routeProvider.when('/NuvIoT', {templateUrl: 'NuvIoT.html'});   
     $routeProvider.when('/NuvIoT.html/Models', {templateUrl: '/partials/Models.html', controller:"ModelController"});
     $routeProvider.otherwise({redirectTo: '/NuvIoT.html'});
 

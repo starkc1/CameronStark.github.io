1d2893c061076b1b7a9b1482c725700dd3e8d675
starkc1 <cms117@live.com>
2016-12-28 14:02:14 -0500

    deleted unused angular

--- js/services/PracticeService.js
+++
@@ -1,9 +0,0 @@
-app.factory('PracticeService', ['$http', function($http) {
-    return $http.get('http://dev.nuviot.com/metadata/dox/domains')
-        .success(function(data) {
-            return data;
-        })
-        .error(function(err) {
-            return err
-        });
-}]);

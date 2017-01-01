bd058266f5e285db9a3ebee27210d357a31cf522
starkc1 <cameronstark@StarkAir.local>
2017-01-01 11:31:31 -0500

    removed files

--- js/controllers/ModelController.js
+++
@@ -1,5 +0,0 @@
-app.controller('ModelController', function($scope, $http, $attrs) {
-    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + id ).then(function(response) {
-        $scope.model = response.data;
-    });
-});

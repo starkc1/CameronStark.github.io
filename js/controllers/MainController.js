d213ff124bfd3eaed99d46c86f33c6778448d4e3
starkc1 <cameronstark@StarkAir.local>
2017-01-04 23:07:36 -0500

    changed experience, controller and added css

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -52,4 +52,28 @@ app.controller('MainController', ['$scope', function($scope) {
             prog: 'AutoCad'
         }
     ];
+
+    $scope.experience = [
+        {
+            company: 'StarkNetwork',
+            img: 'images/StarkNetwork_Logo2.jpg',
+            position: 'Web and App Developer',
+            info1: 'Developed webpages for various customers',
+            info2: 'Developed mobile apps for various customers'
+        },
+        {
+            company: 'Tier-Zero',
+            img: 'images/tier-zero.png',
+            position: 'Lead Web and App Developer',
+            info1: 'Developed UWP apps for Windows 10',
+            info2: 'Developed Windows 8.1 apps'
+        },
+        {
+            company: 'Fusion Advertising Inc.',
+            img: 'images/Fusion_Logo.png',
+            position: 'Lead Mobile Application Developer',
+            info1: 'Developed The Frugal You coupon application',
+            info2: 'Developed the database for coupon storage'
+        },
+    ]
 }]);

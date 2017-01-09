48aedb0a2be2b6382b385d84489886f8809383e1
Cameron Stark <cms117@live.com>
2017-01-08 21:56:26 -0500

    added new report

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -79,12 +79,12 @@ app.controller('MainController', ['$scope', function($scope) {
 
     $scope.report = [
         {
-            period: 'Week of December 17th - December 23rd',
-            hours: 'Time Spent: 15.29 hrs',
-            task1: 'A.) Accomplished setting up the first controller and began work on routing',
-            task2: 'B.) Learned the basics of Angular and how it works with HTML',
-            task3: 'C.) I found that getting the routing to be setup correctly and getting the angular components to work properly within the webpage to be difficult becuase there can be very little feed back from the webpage',
-            task4: 'D.) I enjoyed learning a new language and seeing what it is capable of is very interesting'
+            period: 'Week of January 1st - January 7th',
+            hours: 'Time Spent: 13.58 hrs',
+            task1: 'A.) Accomplished setting up controllers and with assitance completed the passing of parameters for the user to move through the website and JSON URLs',
+            task2: 'B.) Learned how services work with the html and controllers to allow for the passing of parameters between various controllers',
+            task3: 'C.) I found that getting the controller to receive the parameter once passed by the first controller and the service because the way angular can be used varies from each person therefore learning can be a challenge at times',
+            task4: 'D.) I enjoyed trying to figure it out and also seeing the effect of passing parameter by use of the console.log which was cool to see it working'
         },
         {
             period: 'Week of December 25th - December 31st',
@@ -93,6 +93,14 @@ app.controller('MainController', ['$scope', function($scope) {
             task2: 'B.) Learned routing and how it can be used with multiple controllers to create a website',
             task3: 'C.) I found that getting the routing and the ng-view to get working was difficult and passing a value from the view to a controller',
             task4: 'D.) I enjoyed setting up the whole thing because final effect of the siingle wab page layout is very interesting'
-        }
+        },
+        {
+            period: 'Week of December 17th - December 23rd',
+            hours: 'Time Spent: 15.29 hrs',
+            task1: 'A.) Accomplished setting up the first controller and began work on routing',
+            task2: 'B.) Learned the basics of Angular and how it works with HTML',
+            task3: 'C.) I found that getting the routing to be setup correctly and getting the angular components to work properly within the webpage to be difficult becuase there can be very little feed back from the webpage',
+            task4: 'D.) I enjoyed learning a new language and seeing what it is capable of is very interesting'
+        },
     ];
 }]);

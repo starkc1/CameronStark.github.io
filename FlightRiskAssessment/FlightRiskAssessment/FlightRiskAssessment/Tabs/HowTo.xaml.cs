cc7d8905df6434aa53f7f54a2908bb7273cb0326
Cameron Stark <cms117@live.com>
2017-05-26 07:49:28 -0400

    solutun

---
+++ FlightRiskAssessment/FlightRiskAssessment/FlightRiskAssessment/Tabs/HowTo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlightRiskAssessment
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HowTo : ContentPage
+    {
+        public HowTo()
+        {
+            InitializeComponent();
+
+        }
+    }
+
+    
+}
---
+++ FlightRiskAssessment/FlightRiskAssessment/FlightRiskAssessment/Tabs/Tabbed.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace FlightRiskAssessment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tabbed : TabbedPage
+    {
+
+        public Tabbed()
+        {
+            InitializeComponent();
+
+            var navigationPage = new NavigationPage(new Departure());
+            navigationPage.Title = "Simulation";
+
+            Children.Add(navigationPage);
+            Children.Add(new HowTo());
+        }
+    }
+}

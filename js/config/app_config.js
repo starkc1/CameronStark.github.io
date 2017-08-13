5d18405fdd7ad4f3c7bdd190efdd15563493c73c
starkc1 <starkc920@outlook.com>
2017-08-13 18:58:04 -0400

    deleted old site

--- js/config/app_config.js
+++ js/config/app_config.js
@@ -1,25 +1,19 @@
- app.config(['$routeProvider', function($routeProvider) {
-
-    //  $routeProvider
-    //     .when('/Home', {
-    //         templateUrl: 'views/Home.html'
-    //     })
-    //     .when('/Experience', {
-    //         templateUrl: 'views/Experience.html'
-    //     })
-    //     .when('/Projects', {
-    //         templateUrl: 'views/Projects.html'
-    //     })
-    //     .when('/Blog', {
-    //         templateUrl: 'views/Blog.html'
-    //     })
-    //     .when('/Resume', {
-    //         templateUrl: 'views/Resume.html'
-    //     })
-    //     .when('#Navigation', {
-
-    //     })
-    //     .otherwise({
-    //         redirectTo: '/Home'
-    //     })
- }]);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/Home', {
+            templateUrl: 'views/Home.html'
+        })
+        .when('/Experience', {
+            templateUrl: 'views/Experience.html'
+        })
+        .when('/Projects', {
+            templateUrl: 'views/Projects.html'
+        })
+        .when('/Resume', {
+            templateUrl: 'views/Resume.html'
+        })
+        .otherwise({
+            redirectTo: '/Home'
+        })
+        
+}]);

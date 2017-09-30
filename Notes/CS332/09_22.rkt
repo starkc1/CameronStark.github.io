885adf0992edc2fb94d49d51fc1af5952fc76169
starkc1 <starkc920@outlook.com>
2017-09-30 15:27:53 -0400

    yeah

---
+++ Notes/CS332/09_22.rkt
@@ -0,0 +1,11 @@
+;;; Given a sequence of integers, S = <S1, S2, S3...Sn>,
+;;; find the length, l, of the longest sequence in S, Si, Si+1, Si+2,.....2,.....S,
+;;; such that Sj <= Sj+1, j <= i <= k
+
+;;; S = {0,1,2,0,7,43,12,16,18,22,29,0}
+
+;;; int seqCounter = 1;
+;;; int maxCounter = 1;
+;;; int currentVal = S[1];
+
+(+ 2 2)

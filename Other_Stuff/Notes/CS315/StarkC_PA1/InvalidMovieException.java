cc87c83868e9e8d6a8a36ff2308731ee4934b1b1
Cameron Stark <starkc920@outlook.com>
2018-10-07 17:30:05 -0400

    push up new site

---
+++ Other_Stuff/Notes/CS315/StarkC_PA1/InvalidMovieException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * Simple Exception to handle invalid movies.  Doesn't really do anything besides being an exception.
+ * 
+ * @author Richard
+ *
+ */
+public class InvalidMovieException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4496012944354433567L;
+
+}
